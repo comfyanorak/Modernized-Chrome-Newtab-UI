6870386044f53deeacd4a17308dcce260275d989
5P1CY_P1ZZ4 <95450438+5P1CYP1ZZ4@users.noreply.github.com>
2022-12-14 20:08:45 -0500

    added search overlay; added overlay.js to manage overlay scripts

---
+++ overlay.js
@@ -0,0 +1,14 @@
+function openNav1() {
+    document.getElementById("myNav1").style.width = "100%";
+  }
+  
+  function closeNav1() {
+    document.getElementById("myNav1").style.width = "0%";
+  }
+  function openNav2() {
+    document.getElementById("myNav2").style.width = "100%";
+  }
+  
+  function closeNav2() {
+    document.getElementById("myNav2").style.width = "0%";
+  }
