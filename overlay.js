279e2d01ddd06dfcda97bb60700a8155b0aae09a
5P1CY_P1ZZ4 <95450438+5P1CYP1ZZ4@users.noreply.github.com>
2022-12-15 08:32:17 -0500

    updated overlay.js for overlay size

--- overlay.js
+++ overlay.js
@@ -1,17 +1,17 @@
 function openNav1() {
-    document.getElementById("myNav1").style.width = "50%";
+    document.getElementById("myNav1").style.width = "25%";
 }  
 function closeNav1() {
     document.getElementById("myNav1").style.width = "0%";
 }
 function openNav2() {
-    document.getElementById("myNav2").style.width = "50%";
+    document.getElementById("myNav2").style.width = "25%";
 }
 function closeNav2() {
     document.getElementById("myNav2").style.width = "0%";
 }
 function openNav3() {
-    document.getElementById("myNav3").style.width = "50%";
+    document.getElementById("myNav3").style.width = "100%";
 }
 function closeNav3() {
     document.getElementById("myNav3").style.width = "0%";
