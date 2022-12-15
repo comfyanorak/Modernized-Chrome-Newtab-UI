72b85218b4267da291b466d9d02073671350c62b
5P1CY_P1ZZ4 <95450438+5P1CYP1ZZ4@users.noreply.github.com>
2022-12-14 20:12:44 -0500

    updated? overlay.js

--- overlay.js
+++ overlay.js
@@ -1,14 +1,12 @@
 function openNav1() {
     document.getElementById("myNav1").style.width = "100%";
-  }
-  
-  function closeNav1() {
+}  
+function closeNav1() {
     document.getElementById("myNav1").style.width = "0%";
-  }
-  function openNav2() {
+}
+function openNav2() {
     document.getElementById("myNav2").style.width = "100%";
-  }
-  
-  function closeNav2() {
+}
+function closeNav2() {
     document.getElementById("myNav2").style.width = "0%";
-  }
+}
